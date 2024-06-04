645d2910de6ef966ab3c78ca4f95ae2082b11915
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-04 22:51:03 +0545

    chore: Add error handling middleware for API routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import omit from "../utils/omit.js";
+import { errorHandler } from "../utils/error.js";
 class AuthController {
   static login = async (req, res) => {
     const data = await User.find();
     const omittedData = data.map((user) => omit(user._doc, ["password"]));
     res.status(200).json({ message: "Login successful", data: omittedData });
   };
-  static register = async (req, res) => {
+  static register = async (req, res, next) => {
     try {
       const { username, name, email, password } = req.body;
       const hashedPassword = await bcryptjs.hashSync(password, 10);
@@ -21,7 +22,7 @@ class AuthController {
       const omittedUser = omit(newUser._doc, ["password"]);
       res.status(201).json({ message: "User registered", user: omittedUser });
     } catch (error) {
-      res.status(500).json(error.message);
+      next(error);
     }
   };
 }
