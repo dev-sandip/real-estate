29eabd39b1218e3071e7da90e6f954f8391ac2ae
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-04 23:34:58 +0545

    refactor: Remove unused SignOut component and update dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./components/NotFound";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Toaster position="bottom-right" reverseOrder={false} />
       <Header />
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
