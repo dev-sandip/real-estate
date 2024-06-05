6bf1b9b28f876e080b3397710c0100ae13cfa072
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-06-05 19:06:38 +0545

    ✨feature: Add signin and signup functionality with JWT authentication

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,13 +2,43 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import omit from "../utils/omit.js";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 class AuthController {
-  static login = async (req, res) => {
-    const data = await User.find();
-    const omittedData = data.map((user) => omit(user._doc, ["password"]));
-    res.status(200).json({ message: "Login successful", data: omittedData });
+  static signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        return res.json(errorHandler(404, "User not found!"));
+      }
+      const isValidPassword = bcryptjs.compareSync(
+        password,
+        validUser.password
+      );
+      if (!isValidPassword) {
+        return res.json(errorHandler(400, "Invalid credentials!"));
+      }
+      const omittedUser = omit(validUser._doc, ["password"]);
+      const token = jwt.sign(
+        {
+          id: validUser._id,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          sameSite: "strict",
+          secure: process.env.NODE_ENV === "production",
+        })
+        .status(200)
+        .json({ message: "Logged In Successfully!", user: omittedUser });
+    } catch (error) {
+      next(error);
+    }
   };
-  static register = async (req, res, next) => {
+  static signup = async (req, res, next) => {
     try {
       const { username, name, email, password } = req.body;
       const hashedPassword = await bcryptjs.hashSync(password, 10);
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -2,6 +2,6 @@ import express from "express";
 import AuthController from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.get("/login", AuthController.login);
-router.post("/register", AuthController.register);
+router.post("/login", AuthController.signin);
+router.post("/register", AuthController.signup);
 export default router;
